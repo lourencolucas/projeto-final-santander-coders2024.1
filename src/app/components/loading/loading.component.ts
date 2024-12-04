f994e790b9c4de09d3dbe3ffcf7229f5d8b81cc3
Lucas Oliveira <49119918+lourencolucas@users.noreply.github.com>
2024-12-04 20:20:13 -0300

    Create a LoadingComponent

---
+++ src/app/components/loading/loading.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-loading',
+  template: `
+    <div class="loading-container">
+      <div class="loading-spinner"></div>
+    </div>
+  `,
+  styleUrls: ['./loading.component.css']
+})
+export class LoadingComponent {}
