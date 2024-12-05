fdfd5842133289c70e526b638d538b6a6b14b9a0
Lucas Oliveira <49119918+lourencolucas@users.noreply.github.com>
2024-12-05 11:06:54 -0300

    Create and adjust for interceptor auth

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './app/interceptors/auth.interceptor';
+
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])), // Registrar o interceptor
+  ],
 }).catch((err) => console.error(err));
